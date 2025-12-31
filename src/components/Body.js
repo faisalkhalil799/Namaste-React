3f75bf73af8e3e22252cf6bf760713437cb49166
@faisalk <faisal.khalil@group.one>
2025-12-31 13:46:47 +0530

    integrated the live API data and shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+import FakeCard from "./FakeCard";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(resList);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const apiListData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    const restaurantsArray =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setRestaurantList(restaurantsArray);
+  };
 
+  useEffect(() => {
+    apiListData();
+  }, []);
   const buttonClickhandler = () => {
-    const filteredList = resList.filter(
-      (restaurant) => restaurant.info.avgRating > 4
+    const filteredList = restaurantList?.filter(
+      (restaurant) => restaurant.info.avgRating > 4.1
     );
     setRestaurantList(filteredList);
   };
 
-  const resetHandler = () => {
-    setRestaurantList(resList);
-  };
+  if (restaurantList.length === 0) {
+    return <FakeCard />;
+  }
 
   return (
     <>
       <button className="filter-btn" onClick={buttonClickhandler}>
         Top Rated Restaurants
-      </button>
-      <button className="reset-btn" onClick={resetHandler}>
-        Reset
       </button>
       <div className="body-container">
         {restaurantList.map((restaurant) => (
---
+++ src/components/FakeCard.js
@@ -0,0 +1,24 @@
+//Shimmer UI for loading state
+const FakeCard = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
+
+export default FakeCard;
