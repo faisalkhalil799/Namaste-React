066d79fccfc694df9d5b9f5e031e5a83aaa45544
@faisalk <faisal.khalil@group.one>
2026-01-03 19:52:55 +0530

    fetching github data inside the class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,24 +4,39 @@ class AboutClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      userInfo: {
+        name: "Dummy Name",
+        login: "Default",
+        avatar_url:
+          "https://www.freepik.com/free-vector/blue-circle-with-white-user_145857007.htm#fromView=keyword&page=1&position=0&uuid=5a69e9d9-eeda-4e39-b461-698fc4f8a9fc&query=Avatar+profile",
+      },
+      // count: 0,
     };
   }
-
-  increaseCounter = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  async componentDidMount() {
+    const response = await fetch(
+      "https://api.github.com/users/faisalkhalil799"
+    );
+    const json = await response.json();
+    this.setState({
+      userInfo: json,
+    });
+  }
+  // increaseCounter = () => {
+  //   this.setState({ count: this.state.count + 1 });
+  // };
   render() {
-    const { name, twitterHandle } = this.props;
-    const { count } = this.state;
+    // const { name, twitterHandle } = this.props;
+    const { name, login, avatar_url } = this.state.userInfo;
     return (
       <>
         <h1>Coming from class based component</h1>
         <h2>
-          Name:{name}, Twitter Handle: {twitterHandle}
+          Name:{name}, Github User Handle: {login}
         </h2>
-        <div>Count: {count}</div>
-        <button onClick={this.increaseCounter}>Counter</button>
+        <img src={avatar_url} alt="avatar image" />
+        {/* <div>Count: {count}</div> */}
+        {/* <button onClick={this.increaseCounter}>Counter</button> */}
       </>
     );
   }
