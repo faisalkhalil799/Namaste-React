de7dffcf3a8a691e24ed58f8342ad97c557dff2c
@faisalk <faisal.khalil@group.one>
2026-01-04 11:28:58 +0530

    Lazy loaded the cart component for reducing the bundle size

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./components/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Body from "./components/Body";
 import Contact from "./components/Contact";
-import Cart from "./components/Cart";
 import ErrorBoundary from "./components/ErrorBoundary";
 import About from "./components/About";
 import MenuCard from "./components/MenuCard";
+import FakeCard from "./components/FakeCard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
+const Cart = lazy(() => {
+  return import("./components/Cart");
+});
 const routes = createBrowserRouter([
   {
     path: "/",
@@ -20,7 +22,14 @@ const routes = createBrowserRouter([
       { path: "/", element: <Body /> },
       { path: "/about", element: <About /> },
       { path: "/contact", element: <Contact /> },
-      { path: "/cart", element: <Cart /> },
+      {
+        path: "/cart",
+        element: (
+          <Suspense fallback={<FakeCard />}>
+            <Cart />
+          </Suspense>
+        ),
+      },
       { path: "restaurant/:resId", element: <MenuCard /> },
     ],
   },
