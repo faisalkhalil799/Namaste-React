122a31b42a058f0e965ca3d9afe004848d675d07
@faisalk <faisal.khalil@group.one>
2026-01-09 23:10:10 +0530

    border sticked to the bottom for less cards as well

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="w-full bg-gray-100 border-t border-gray-200 mt-8">
+    <footer className="w-full bg-gray-100 border-t border-gray-200 mt-8 fixed bottom-0">
       <p className="text-center text-sm text-gray-600 py-4">
         © 2024 My Website. All rights reserved.
       </p>
